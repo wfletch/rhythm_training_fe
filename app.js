1da8a8aff93d5a0d1b00f8a371b4796e80fa1e80
Warren Fletcher <warren@fletch.band>
2022-12-03 00:27:21 +0100

    Added ZMQ Requirement

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const zmq = require("zeromq");
 // const bootstrap = require('bootstrap');
 const app = express()
 const port = 3000
