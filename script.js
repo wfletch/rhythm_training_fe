af65f39ac5a68e7f59d3decc90738a35cdaba47c
Warren Fletcher <warren@fletch.band>
2022-12-06 07:31:51 +0100

    Clean Up

--- script.js
+++ script.js
@@ -3,53 +3,15 @@ function local_function() {
     url: "/update",
     context: document.body,
   }).done(function (data) {
-    console.log("DONE?")
     // $(this).addClass("done");
     // let text = "beat_";
     let num = data["current_beat"] + 1;
     // let beat = texct.concat(num);
-    console.log(data);
 
     $(".beat-box").each(function (index) {
       $(this).removeClass("btn-success");
     });
     $("#beat_" + num).addClass("btn-success");
-    //     if (num === 1) {
-    //       document
-    //         .getElementById("beat_1")
-    //         .setAttribute("class", "btn btn-success");
-    //     } else {
-    //       document
-    //         .getElementById("beat_1")
-    //         .setAttribute("class", "btn btn-secondary");
-    //     }
-    //     if (num === 2) {
-    //       document
-    //         .getElementById("beat_2")
-    //         .setAttribute("class", "btn btn-success");
-    //     } else {
-    //       document
-    //         .getElementById("beat_2")
-    //         .setAttribute("class", "btn btn-secondary");
-    //     }
-    //     if (num === 3) {
-    //       document
-    //         .getElementById("beat_3")
-    //         .setAttribute("class", "btn btn-success");
-    //     } else {
-    //       document
-    //         .getElementById("beat_3")
-    //         .setAttribute("class", "btn btn-secondary");
-    //     }
-    //     if (num === 4) {
-    //       document
-    //         .getElementById("beat_4")
-    //         .setAttribute("class", "btn btn-success");
-    //     } else {
-    //       document
-    //         .getElementById("beat_4")
-    //         .setAttribute("class", "btn btn-secondary");
-    //     }
   });
 }
 
